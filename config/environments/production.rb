0d5c3b33989f848190dedf02010fa92690c4129e
cakejelly <jake.kelly10@gmail.com>
2014-07-15 13:55:30 +0100

    okies

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,10 +26,10 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # # Do not fallback to assets pipeline if a precompiled asset is missed.
+  # config.assets.compile = true
 
-  config.assets.initialize_on_precompile = false
+  # config.assets.initialize_on_precompile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
